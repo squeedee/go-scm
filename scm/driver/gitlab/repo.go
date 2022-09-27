fa26a8e757af3d24a44ad31b3a7b5a17497cd023
Rasheed Abdul-Aziz <rabdulaziz@vmware.com>
2022-09-27 08:34:03 -0400

    WIP

--- scm/driver/gitlab/repo.go
+++ scm/driver/gitlab/repo.go
@@ -459,8 +459,9 @@ func (s *repositoryService) DeleteHook(ctx context.Context, repo, id string) (*s
 	return s.client.do(ctx, "DELETE", path, nil, nil)
 }
 
-func (s *repositoryService) Delete(context.Context, string) (*scm.Response, error) {
-	return nil, scm.ErrNotSupported
+func (s *repositoryService) Delete(ctx context.Context, repo string) (*scm.Response, error) {
+	path := fmt.Sprintf("api/v4/projects/%s", encode(repo))
+	return s.client.do(ctx, "DELETE", path, nil, nil)
 }
 
 // helper function to convert from the gogs repository list to
